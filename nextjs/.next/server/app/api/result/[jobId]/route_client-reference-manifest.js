374bd027edb055519ca81829cf1e691e1db208d0
Matt Frayser <98035703+MattFrayser@users.noreply.github.com>
2025-11-16 16:08:42 -0500

    Delete nextjs/.next directory

--- nextjs/.next/server/app/api/result/[jobId]/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/result/[jobId]/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
--- nextjs/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sBVLViOO8g5SpGSE3DTdLUEQAQ+0h17psRq1YudPsXI=\"\n}"
